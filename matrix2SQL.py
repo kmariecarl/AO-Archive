fb714ca823f25f899330cc12aa069f8e102160c3
carl4498 <carl4498@umn.edu>
2018-03-15 10:35:37 -0500

    add if not exists and comments

--- matrix2SQL.py
+++ matrix2SQL.py
@@ -4,6 +4,11 @@
 #A fifth column is added which converts the deptime to seconds for use in the TTMatrixLinkSQL.py script
 #As it stands, this script creates a new db if given a new file name. If given the existing path and db
 #name, the script will raise an exception and no changes will be commited.
+
+#Example Usage: kristincarlson~ python matrix2SQL.py -path /Users/kristincarlson/Dropbox/Bus-Highway/Task3/
+# Restart2/IntermodalAccess/1_TTMatrixLink/ -name newdbname.sqlite -table1 o2pnr -data1 <path/file.txt>
+#-table2 pnr2d -data2 <path/file.txt>
+
 import sqlite3
 import csv
 import argparse
@@ -29,7 +34,7 @@ if __name__ == '__main__':
     parser.add_argument('-data2', '--TABLE2_PATH_DATA', required=True, default=None)  #List path to .csv file
     args = parser.parse_args()
 
-
+    #Make db
     sqlite_file = '{}{}'.format(args.FOLDER_PATH, args.DB_NAME)
 
     con = sqlite3.connect(sqlite_file)
@@ -37,50 +42,45 @@ if __name__ == '__main__':
 
     # Create a new table in the SQLite database where the o2pnr data will go.
     try:
-        cur.execute('CREATE TABLE {} (origin VARCHAR, destination VARCHAR, deptime VARCHAR, traveltime BIGINT);'.format(args.TABLE1_NAME))
-        cur.execute('CREATE INDEX o2pnr_deptime_origin ON {} (deptime ASC, origin ASC);'.format(args.TABLE1_NAME))
+        cur.execute('CREATE TABLE IF NOT EXISTS {} (origin VARCHAR, destination VARCHAR, deptime VARCHAR, traveltime BIGINT);'.format(args.TABLE1_NAME))
 
     except sqlite3.OperationalError:
-        print('Error: Table already exists in sqlite db.')
-        print('indicator1')
+        print('Error: Please debug')
 
     #Open the data for table 1 into a DictReader Object
     with open('{}'.format(args.TABLE1_PATH_DATA)) as mydata:
         ttmatrix1 = csv.DictReader(mydata)
-
-        to_db = [(row['origin'], row['destination'], row['deptime'], row['traveltime']) for row in ttmatrix1]
+        #Produce list for each row of input data
+        to_db_tbl1 = [(row['origin'], row['destination'], row['deptime'], row['traveltime']) for row in ttmatrix1]
 
     #Batch process the data into table 1
     try:
         cur.executemany(
             "INSERT INTO {} (origin, destination, deptime, traveltime) VALUES (?, ?, ?, ?);".format(args.TABLE1_NAME),
-            to_db)
+            to_db_tbl1)
     except sqlite3.OperationalError:
         print('Error: Values already inserted to {}.'.format(args.TABLE1_NAME))
-        print('indicator2')
 
     # Create a new table in the SQLite database where the pnr2d data will go.
     try:
-        cur.execute('CREATE TABLE {} (origin VARCHAR, destination VARCHAR, deptime VARCHAR, traveltime BIGINT);'
+        cur.execute('CREATE TABLE IF NOT EXISTS {} (origin VARCHAR, destination VARCHAR, deptime VARCHAR, traveltime BIGINT);'
                     .format(args.TABLE2_NAME))
-        cur.execute('CREATE INDEX pnr2d_deptime_origin ON {} (deptime ASC, origin ASC);'.format(args.TABLE2_NAME))
+
     except sqlite3.OperationalError:
-        print('Error: Table already exists in sqlite db.')
-        print('indicator3')
+        print('Error: Please debug.')
 
     #Open the data for table 2 into a DictReader Object
     with open('{}'.format(args.TABLE2_PATH_DATA)) as mydata:
         ttmatrix2 = csv.DictReader(mydata)
-        to_db = [(row['origin'], row['destination'], row['deptime'], row['traveltime']) for row in ttmatrix2]
+        to_db_tbl2 = [(row['origin'], row['destination'], row['deptime'], row['traveltime']) for row in ttmatrix2]
 
     #Batch process the data into table 1
     try:
         cur.executemany(
             "INSERT INTO {} (origin, destination, deptime, traveltime) VALUES (?, ?, ?, ?);".format(args.TABLE2_NAME),
-            to_db)
+            to_db_tbl2)
     except sqlite3.OperationalError:
         print('Error: Values already inserted to {}.'.format(args.TABLE2_NAME))
-        print('indicator4')
 
     # Add a column to table 1 & 2 to translate deptimes into an integer in seconds.
     try:
@@ -92,9 +92,14 @@ if __name__ == '__main__':
 
     except sqlite3.OperationalError:
         print('Error: Column data already exists in tables.')
-        print('indicator5')
 
+    #Create Indices on the new deptime_sec column of the tables.
+    try:
+        cur.execute('CREATE INDEX o2pnr_deptime_origin ON {} (deptime_sec ASC, origin ASC);'.format(args.TABLE1_NAME))
+        cur.execute('CREATE INDEX pnr2d_deptime_origin ON {} (deptime_sec ASC, origin ASC);'.format(args.TABLE2_NAME))
+    except sqlite3.OperationalError:
+        print('Error: Indices already created')
 
-    # #Committing change and closing the connection to the database file.
+    #Committing change and closing the connection to the database file.
     con.commit()
     con.close()
