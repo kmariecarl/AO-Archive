509c1d14c8b983f090c8cdb21f2101c426022704
carl4498 <carl4498@umn.edu>
2018-04-09 11:17:59 -0500

    Create o2pnr table and load data for large TTmatrix

---
+++ copy_o2pnr_totable.sql
@@ -0,0 +1,3 @@
+﻿--CREATE TABLE ttmatrices.o2pnr (origin VARCHAR, destination varchar, deptime smallint, traveltime integer);
+--COPY ttmatrices.o2pnr from '/Users/kristincarlson/Dropbox/Bus-Highway/Task3/Restart2/IntermodalAccess/1_TTMatrixLink/Auto_O_PNR_TT_Calc-results.csv' delimiter ',' csv header;
+--select count(*) from ttmatrices.o2pnr;
