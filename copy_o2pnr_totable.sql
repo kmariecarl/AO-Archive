46109e6477d411cfc1d1dc02cace7f0518429834
carl4498 <carl4498@umn.edu>
2018-04-16 12:50:45 -0500

    Added indexing to table creation

--- copy_o2pnr_totable.sql
+++ copy_o2pnr_totable.sql
@@ -1,3 +1,4 @@
 ﻿--CREATE TABLE ttmatrices.o2pnr (origin VARCHAR, destination varchar, deptime smallint, traveltime integer);
 --COPY ttmatrices.o2pnr from '/Users/kristincarlson/Dropbox/Bus-Highway/Task3/Restart2/IntermodalAccess/1_TTMatrixLink/Auto_O_PNR_TT_Calc-results.csv' delimiter ',' csv header;
 --select count(*) from ttmatrices.o2pnr;
+CREATE INDEX origin_deptime ON ttmatrices.o2pnr (origin, deptime);
