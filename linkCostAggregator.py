8327a91f48b071e2af08950d523cdf0ecf5f4fc4
carl4498 <carl4498@umn.edu>
2018-04-24 11:00:06 -0500

    Add new cost fields and scenarios

--- linkCostAggregator.py
+++ linkCostAggregator.py
@@ -1,5 +1,12 @@
 #This script finds the total cost of a series of links between an OD pair based on the linkCostCalculator.py output
 
+#Need to add fixed costs and trip based parking costs and maybe mnpass costs to the path output, because these fixed costs
+#are trip based!!
+
+#Need to implement scenario columns
+
+#This code looks ugly but it works very well.
+
 #################################
 #           IMPORTS             #
 #################################
@@ -22,38 +29,107 @@ if __name__ == '__main__':
     # Parameterize file paths
     parser = argparse.ArgumentParser()
     parser.add_argument('-file', '--LINK_COST_FILE', required=True, default=None)  #ENTER AS full file path to path_analyst file
+    parser.add_argument('-fixed', '--TIME_DEP_FEE_FIXED_COST', required=True, default=267) #The average fixed monetary cost per veh-year is $3,893.50, equivalent
+    #to $2.67/veh-trip assuming average annual mileage, n
+    #Later scenarios to include parking and mnpass options
+    # parser.add_argument('-park_contract', '--PARKING_CONTRACT_FIXED_COST', required=True, default=000) #NA
+    # parser.add_argument('-park_daily', '--PARKING_DAILY_FIXED_COST', required=True, default=000) #NA
+    # parser.add_argument('-mnpass', '--MNPASS_FIXEDD_COST', required=True, default=000) #NA
+
+
     args = parser.parse_args()
 
     #Read in link cost file
     reader = mod.readInToDict(args.LINK_COST_FILE)
     #Initiate writer file
-    fieldnames = ['origin', 'deptime', 'destination', 'path_cost']
+    fieldnames = ['origin', 'deptime', 'destination', 'fuel_cost', 'repair_cost', 'depreciation_cost', 'irs_cost',
+                  'vot_cost', 'fix_cost', 'A', 'B', 'C', 'D', 'E', 'F', 'G']
     writer = mod.mkDictOutput('Path_Cost_Matrix_{}'.format(curtime), fieldname_list=fieldnames)
     #Initiate variables
+    FIXED = float(args.TIME_DEP_FEE_FIXED_COST)
+
     count = 0
-    sumlink = 0
+    sumfuel = 0
+    sumrep = 0
+    sumdep = 0
+    sumirs = 0
+    sumvot = 0
+    sumfix = 0
+    suma = 0
+    sumb = 0
+    sumc = 0
+    sumd = 0
+    sume = 0
+    sumf = 0
+    sumg = 0
+
     previous = {}
+
     for row in reader:
-        current = {'origin': row['origin'], 'deptime': row['deptime'], 'destination': row['destination'], 'path_cost': sumlink}
+        current = {'origin': row['origin'], 'deptime': row['deptime'], 'destination': row['destination'],
+                   'fuel_cost': sumfuel, 'repair_cost': sumrep, 'depreciation_cost': sumdep, 'irs_cost': sumirs, 'vot_cost': sumvot,
+                   'fix_cost': FIXED, 'A': suma, 'B': sumb, 'C': sumc, 'D': sumd, 'E': sume, 'F': sumf, 'G': sumg}
         #Scenario to handle the first OD pair
         if int(row['path_seq']) == 0 and count == 0:
-            sumlink += float(row['link_cost'])
+            sumfuel += float(row['fuel_cost'])
+            sumrep  += float(row['repair_cost'])
+            sumdep += float(row['depreciation_cost'])
+            sumirs += float(row['irs_cost'])
+            sumvot += float(row['vot_cost'])
+
             previous = current
         #Row only written when a sequence has been finished
         elif int(row['path_seq']) == 0 and count != 0:
+            #At the end of the iteration, add up the sums of each variable for the different scenarios
+            suma = sumfuel + sumrep
+            sumb = sumfuel + sumdep
+            sumc = sumfuel + sumrep + sumdep
+            sumd = sumfuel + sumrep + sumdep +sumvot
+            sume = sumfuel + sumrep + sumvot
+            sumf = sumfuel + sumdep + sumvot
+            sumg = sumfuel + FIXED
+            #Update the previous row with the sums that were found for this run through.
+            previous['A'] = suma
+            previous['B'] = sumb
+            previous['C'] = sumc
+            previous['D'] = sumd
+            previous['E'] = sume
+            previous['F'] = sumf
+            previous['G'] = sumg
+
             writer.writerow(previous)
             #Reset sumlink to zero
-            sumlink = 0
-            sumlink += float(row['link_cost'])
+            sumfuel = 0
+            sumfuel += float(row['fuel_cost'])
+            sumrep = 0
+            sumrep  += float(row['repair_cost'])
+            sumdep = 0
+            sumdep += float(row['depreciation_cost'])
+            sumirs = 0
+            sumirs += float(row['irs_cost'])
+            sumvot = 0
+            sumvot += float(row['vot_cost'])
             previous = current
             #Update previous with the sumlink that was found for this run through.
-            previous['path_cost'] = sumlink
+            previous['fuel_cost'] = sumfuel
+            previous['repair_cost'] = sumrep
+            previous['depreciation_cost'] = sumdep
+            previous['irs_cost'] = sumirs
+            previous['vot_cost'] = sumvot
         #Most times the Else clause will catch
         else:
-            sumlink += float(row['link_cost'])
+            sumfuel += float(row['fuel_cost'])
+            sumrep  += float(row['repair_cost'])
+            sumdep += float(row['depreciation_cost'])
+            sumirs += float(row['irs_cost'])
+            sumvot += float(row['vot_cost'])
             previous = current
             #Update the previous row with the sumlink that was found for this run through.
-            previous['path_cost'] = sumlink
+            previous['fuel_cost'] = sumfuel
+            previous['repair_cost'] = sumrep
+            previous['depreciation_cost'] = sumdep
+            previous['irs_cost'] = sumirs
+            previous['vot_cost'] = sumvot
         count += 1
 
 
--- linkCostCalculator.py
+++ linkCostCalculator.py
@@ -21,16 +21,34 @@ import csv
 #################################
 #           FUNCTIONS           #
 #################################
-def mpgFunction(speed):
-    #start by converting speed from m/s to miles per hour
-    speed_mph = float(speed) * 2.23694
-    mpg = 0.658 + (0.947 * speed_mph) - (0.009 * speed_mph**2)
-    print('MPG', mpg)
-    return mpg
+def convertLinkLength(link_length_km):
+    #Convert incoming link length from meters to miles
+    mi = float(link_length_km) * 0.000621371
+    return mi
 
-def mpgAdjustment(mpg):
-    mpg_adj = mpg * ADJ
-    print('MPG_ADJ', mpg_adj)
+def convertLinkSpeed(link_speed_m_s):
+    #start by converting speed from m/s to miles per hour
+    speed_mph = float(link_speed_m_s) * 2.23694
+    return speed_mph
+
+def calcFuelCost(row, length_mi, speed_mph):
+    #Calc the MPG based on Mengying's formula as a function of link speed
+    mpg = mpgFunction(speed_mph)
+    #Incorporate average retail fuel price
+    cost_mile = costPerMile(mpg)
+    #Multiply by link length
+    link_cost = removeSciNot(calcLinkCost(length_mi, cost_mile))
+    return link_cost
+
+def mpgFunction(speed_mph):
+
+    if speed_mph > 45:
+        adj = 1.170
+    else:
+        adj = 0.861
+    mpg = 0.658 + (0.947 * speed_mph) - (0.009 * speed_mph**2)
+    mpg_adj = mpg * adj
+    print('MPG_adj', mpg_adj)
     return mpg_adj
 
 def costPerMile(mpg):
@@ -39,14 +57,43 @@ def costPerMile(mpg):
     print('cost per mile', cost_per_mile)
     return cost_per_mile
 
-def calcLinkCost(link_length, cost_mile):
-    #Convert incoming link length from meters to miles
-    mi = float(link_length) * 0.000621371
-    cost = cost_mile * mi
+
+def calcLinkCost(length_mi, cost_mile):
+    cost = cost_mile * length_mi
     return cost
+
 def removeSciNot(value):
     reformat = '{:.10f}'.format(value)
     return reformat
+
+
+def calcRepairCost(row, speedMPH, length_mi):
+    if speedMPH > 45:
+        repair_link_cost = length_mi * REP_HWY
+
+    else:
+        repair_link_cost = length_mi * REP_CITY
+    return repair_link_cost
+
+def calcDepreciationCost(speed_mph, length_mi):
+    if speed_mph > 45:
+        dep_link_cost = length_mi * DEP_HWY
+    else:
+        dep_link_cost = length_mi * DEP_CITY
+
+    return dep_link_cost
+
+def calcIRSCost(lenght_mi):
+    irs_cost = lenght_mi * IRS
+    return irs_cost
+
+
+def calcVOTCost(link_time_sec):
+    #convert link time from seconds to hours
+    person_hour = float(link_time_sec)/3600
+    vot_cost = person_hour * VOT #result will be in cents
+    return vot_cost
+
 
 #################################
 #           OPERATIONS          #
@@ -60,29 +107,47 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-path', '--PATH_FILE', required=True, default=None)  #ENTER AS full file path to path_analyst file
     parser.add_argument('-mpg_adj', '--MPG_ADJUSTMENT', required=True, default=32400)  #Assume city driving adjustment of 0.8354
-    parser.add_argument('-price_per_gal', '--PRICE_PER_GALLON_REGULAR', required=True, default=32400)  # $2.327 in MN in 2017, enter in cents (232.7 cents)
+    parser.add_argument('-price_per_gal', '--PRICE_PER_GALLON_REGULAR', required=True, default=232.7)  # $2.327 in MN in 2017, enter in cents (232.7 cents)
+    parser.add_argument('-vot', '--VALUE_OF_TIME', required=True, default=1803.0)  # MnDOT research states $18.30 or 1803.0 cents
+    parser.add_argument('-repaircity', '--REPAIR_COST_CITY', required=True, default=1.932) #Combined (auto + pickup) city Maintenance and Repair costs in cents per veh-mi for 2015
+    parser.add_argument('-repairhwy', '--REPAIR_COST_HIGHWAY', required=True, default=1.703) #Combined (auto + pickup) highway Maintenance and Repair costs in cents per veh-mi for 2015
+    parser.add_argument('-depcity', '--DEPRECIATION_COST_CITY', required=True, default=3.00) #Combined city vehicle depreciation cost for 2015
+    parser.add_argument('-dephwy', '--DEPRECIATION_COST_HIGHWAY', required=True, default=2.55) #Combined highway vehicle depreciation cost for 2015
+    parser.add_argument('-irs', '--IRS_MILEAGE', required=True, default=53.5) #IRS mileage reimbursement which includes fuel and avg vehicle wear and tear (i.e. depreciation by distance)
+
     args = parser.parse_args()
 
     #Read in constants
     ADJ = float(args.MPG_ADJUSTMENT)
     PRICE = float(args.PRICE_PER_GALLON_REGULAR)
+    VOT = float(args.VALUE_OF_TIME)
+    REP_CITY = float(args.REPAIR_COST_CITY)
+    REP_HWY = float(args.REPAIR_COST_HIGHWAY)
+    DEP_CITY = float(args.DEPRECIATION_COST_CITY)
+    DEP_HWY = float(args.DEPRECIATION_COST_HIGHWAY)
+    IRS = float(args.IRS_MILEAGE)
 
 
     reader = mod.readInToDict(args.PATH_FILE)
     #Make writer
-    fieldnames = ['origin', 'deptime', 'destination', 'path_seq', 'link_cost']
+    fieldnames = ['origin', 'deptime', 'destination', 'path_seq', 'fuel_cost', 'repair_cost', 'depreciation_cost', 'irs_cost', 'vot_cost']
     writer = mod.mkDictOutput('link_costs_matrix_{}'.format(curtime), fieldname_list=fieldnames)
 
 
     for row in reader:
-        #Calc the MPG based on Mengying's formula as a function of link speed
-        mpg = mpgAdjustment(mpgFunction(row['link_speed']))
-        #Incorporate average retail fuel price
-        cost_mile = costPerMile(mpg)
-        #Multiply by link length
-        link_cost = removeSciNot(calcLinkCost(row['link_length'], cost_mile))
+        lengthMI = convertLinkLength(row['link_length']) #starts in km need miles
+        speedMPH = convertLinkSpeed(row['link_speed']) #Starts in m need miles
+        fuelCost = calcFuelCost(row, lengthMI, speedMPH) #Fuel price = f(speed) * price/gallon
+
+        repairCost = calcRepairCost(row, speedMPH, lengthMI)
+        depCost = calcDepreciationCost(speedMPH, lengthMI)
+        irsCost = calcIRSCost(lengthMI)
+        votCost = calcVOTCost(row['link_time'])
+
+
         #Write entry, remember that the written price per link is in cents
-        entry = {'origin': row['origin'], 'deptime': row['deptime'], 'destination': row['destination'], 'path_seq': row['path_seq'], 'link_cost': link_cost}
+        entry = {'origin': row['origin'], 'deptime': row['deptime'], 'destination': row['destination'], 'path_seq': row['path_seq'], 'fuel_cost': fuelCost,
+                 'repair_cost': repairCost, 'depreciation_cost': depCost, 'irs_cost': irsCost, 'vot_cost': votCost}
         writer.writerow(entry)
     print('Link cost analysis finished')
     mod.elapsedTime(start_time)
