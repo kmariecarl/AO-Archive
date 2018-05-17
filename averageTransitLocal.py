8aca9e6c1f2255d444c23648ca579c7955066bcd
carl4498 <carl4498@umn.edu>
2018-05-17 15:32:53 -0500

    Improved compatability with large access inputs and added arguement to indicate jobs column name

--- averageTransitLocal.py
+++ averageTransitLocal.py
@@ -6,7 +6,7 @@
 # Create an empty folder called "Access_Results_Avg"
 # Place this program inside the directory in which these two folders reside.
 # Navigate in the command line to the directory where this program and two folders are located.
-# Run the command "python average_transit_local.py -r input -o output"
+# Run the command "python average_transit_local.py -r input -o output -label Jobs/C000"
 # "input" is the name of folder where the zipped access results are.
 # "output" is the name of the empty "Access_Results_Avg" folder.
 #
@@ -31,8 +31,9 @@ class Timer:
         return datetime.datetime.now() - self.start_time
 
 def createdb(con):
+    print('Creating DB Table')
     cur = con.cursor()
-    cur.execute("CREATE TABLE data (label text, deptime integer, threshold integer, jobs integer)")
+    cur.execute("CREATE TABLE data (label text, deptime integer, threshold integer, {} integer)".format(JOBS_LABEL))
     con.commit()
 
 def dropdb(con):
@@ -40,7 +41,9 @@ def dropdb(con):
     cur.execute("DROP TABLE data;")
     con.commit()
 
-def extract_data(infile):
+def extract_data_and_load(infile, con):
+    print('Extacting data from zipfile')
+    cur = con.cursor()
     zf = ZipFile(infile)
     contents = zf.namelist()
     for name in contents:
@@ -50,13 +53,19 @@ def extract_data(infile):
             print(i)
             reader = csv.reader(i)
             header = next(reader)
-            return [[row[i] for i in [0, 1, 2, 3]] for row in reader]
+            #return [[row[i] for i in [0, 1, 2, 3]] for row in reader]
 
-def load(infile, con):
-    data = extract_data(infile)
-    cur = con.cursor()
-    cur.executemany("INSERT INTO data VALUES (?,?,?,?)", data)
+            for row in reader:
+                data = [row[0], row[1], row[2], row[3]]
+                cur.execute("INSERT INTO data VALUES (?,?,?,?)", data)
     con.commit()
+
+# def load(infile, con):
+#     print('Inserting values to DB table')
+#     data = extract_data(infile)
+#     cur = con.cursor()
+#     cur.executemany("INSERT INTO data VALUES (?,?,?,?)", data)
+#     con.commit()
 
 def create_indices(con):
     print("Indexing data table...")
@@ -68,8 +77,8 @@ def create_indices(con):
 def averages(con):
     print("Calculating averages...")
     cur = con.cursor()
-    cur.execute("""SELECT label, threshold, CAST(avg(jobs) as integer)
-					FROM data GROUP BY label, threshold;""")
+    cur.execute("""SELECT label, threshold, CAST(avg({}) as integer)
+					FROM data GROUP BY label, threshold;""".format(JOBS_LABEL))
     data = cur.fetchall()
     return data
 
@@ -94,7 +103,7 @@ def process(results_directory, output_dir):
         print('Processing averages for file {}...'.format(infile))
         with sqlite3.connect(":memory:") as sqlite_con:
             createdb(sqlite_con)
-            load(infile, sqlite_con)
+            extract_data_and_load(infile, sqlite_con)
             outfile = os.path.join(output_dir,"{}.csv".format(make_avg_filename(os.path.splitext(os.path.basename(infile))[0])))
             write_averages(sqlite_con, outfile)
             print("\n 	Wrote averages to {} for infile {}".format(outfile, infile))
@@ -104,7 +113,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--results_directory', required=True, default=None)
     parser.add_argument('-o', '--output_dir', required=True, default=None)
+    parser.add_argument('-label', '--JOBS_LABEL', required=True, default=None)
     args = parser.parse_args()
+
+    JOBS_LABEL = args.JOBS_LABEL
 
     t = Timer()
     process(args.results_directory, args.output_dir)
