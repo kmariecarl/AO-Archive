5d85a0b4ff2de8c0ac56f9ef7915862b8593ff74
carl4498 <carl4498@umn.edu>
2018-07-18 16:07:36 -0500

    No change

--- linkCostCalculator.py
+++ linkCostCalculator.py
@@ -109,8 +109,9 @@ if __name__ == '__main__':
     start_time, curtime = mod.startTimer()
     readable = time.ctime(start_time)
     print(readable)
-    bar = bar.Bar(message ='Processing', fill='@', suffix='%(percent)d%%', max=51813000) #For 150 links x 30303 x 1 x 114 = 7.65 GB
+    bar = bar.Bar(message ='Processing', fill='@', suffix='%(percent)d%%', max=1377135000)
     #List of approximate line counts for different file sizes:
+    # max=51813000 For 150 links x 30303 x 1 x 114 = 7.65 GB
     #max = 110696205 for 300 links x 3027 x 1 x 114 = 7.65 GB
     #max = 261617092 For 60 links x 30303 x 13 x 114 = 18 GB input file
 
