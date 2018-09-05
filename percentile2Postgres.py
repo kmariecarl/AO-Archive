778a603204dda1cc22e5df6e0deefa9566f6359f
carl4498 <carl4498@umn.edu>
2018-09-05 09:22:52 -0500

    improve index creation

--- percentile2Postgres.py
+++ percentile2Postgres.py
@@ -1,6 +1,6 @@
 #Use this script to load the 15th percentile files or min files by PNR into a single Pnr2d15 or pnr2d15min table within a PostgreSQL db.
 
-#Example Usage: kristincarlson~ python ~/..TTMatrixLink_Testing -db TTMatrixLink -schema test -table3 pnr2d15 -path ./UnitTest_Percentile/
+#Example Usage: kristincarlson~ python ~/..TTMatrixLink_Testing -db TTMatrixLink -schema test -table3 pnr2d15 -path /Volumes/Thang_2/Task3_Extension/Transit_PNR_D_TT_0.2.2-all/Transit_PNR_D_TT_78_Calc/PNR_MinTT
 
 #################################
 #           IMPORTS             #
@@ -14,17 +14,17 @@ import psycopg2
 #################################
 
 def createExtensionList(pnr_list_file):
-    pnr_list = mod.readList(pnr_list_file)
+    pnr_list = mod.readList(pnr_list_file, 'integer')
     extension_list = []
     for pnr in pnr_list:
-        extension_name = 'PNR15_{}_destination.txt'.format(pnr)
+        extension_name = 'PNR_{}_MinTT.csv'.format(pnr)
         extension_list.append(extension_name)
     return extension_list
 
 
 
 def createTable():
-    query = "CREATE TABLE IF NOT EXISTS {}.{} (origin VARCHAR, destination VARCHAR, deptime SMALLINT, traveltime INTEGER)"
+    query = "CREATE TABLE IF NOT EXISTS {}.{} (origin INTEGER, destination BIGINT, deptime CHAR(4), traveltime INTEGER)"
     cur.execute(query.format(SCHEMA, TABLE3))
     print(cur.mogrify(query.format(SCHEMA, TABLE3)))
 
@@ -34,13 +34,26 @@ def insertFileInfo(extension_list):
         cur.execute(query.format(SCHEMA, TABLE3, PATH, extension))
         print(cur.mogrify(query.format(SCHEMA, TABLE3, PATH, extension)))
         mod.elapsedTime(start_time)
+def addDeptimeSec():
+    query = "ALTER TABLE {}.{} ADD COLUMN deptime_sec integer; UPDATE {}.{} SET deptime_sec = cast(substring(deptime, 1, 2) as integer) *3600 + cast(substring(deptime, 3, 4) as integer) * 60 "
+    cur.execute(query.format(SCHEMA, TABLE3, SCHEMA, TABLE3))
+    print(cur.mogrify(query.format(SCHEMA, TABLE3, SCHEMA, TABLE3)))
 
 def createIndex():
-    print('Creating index origin_deptime')
-    query = "CREATE INDEX origin_deptime ON {}.{} (origin, deptime);"
-    cur.execute(query.format(SCHEMA, TABLE3))
-    print(cur.mogrify(query.format(SCHEMA, TABLE3)))
-    print('Index origin_deptime added to table {}'.format(TABLE3))
+    print('Creating index on origin')
+    query1 = "CREATE INDEX origin_pnr2dmin ON {}.{} (origin);"
+    cur.execute(query1.format(SCHEMA, TABLE3))
+
+    query2 =  "CREATE INDEX deptime_sec_pnr2dmin ON {}.{} (deptime_sec);"
+    cur.execute(query2.format(SCHEMA, TABLE3))
+    print(cur.mogrify(query2.format(SCHEMA, TABLE3)))
+    print('Index deptime_sec_pnr2dmin added to table {}'.format(TABLE3))
+
+    query3 =  "CREATE INDEX destination_sec_pnr2dmin ON {}.{} (destination);"
+    cur.execute(query3.format(SCHEMA, TABLE3))
+    print(cur.mogrify(query3.format(SCHEMA, TABLE3)))
+    print('Index deptime_sec_pnr2dmin added to table {}'.format(TABLE3))
+
 
 #################################
 #           OPERATIONS          #
@@ -56,7 +69,7 @@ if __name__ == '__main__':
     parser.add_argument('-db', '--DB_NAME', required=True, default=None)  #ENTER AS TTMatrixLink
     parser.add_argument('-schema', '--SCHEMA_NAME', required=True, default=None)  # ENTER AS ttmatrices
     parser.add_argument('-table3', '--TABLE3_NAME', required=True, default=None)  #Table 3 in schema, i.e. pnr2d15 or pnr2dmin
-    parser.add_argument('-path', '--FILE_PATH', required=True, default=None)  #The file path where all the percentile files are stored.
+    parser.add_argument('-path', '--FILE_PATH', required=True, default=None)  #The entire file path where all the percentile files are stored.
     parser.add_argument('-pnr', '--PNR_LIST', required=True, default=None)  #The file that stores a list of PNRs for use in creating extensions
     args = parser.parse_args()
 
@@ -78,4 +91,5 @@ if __name__ == '__main__':
     extensionList = createExtensionList(PNRList)
     createTable()
     insertFileInfo(extensionList)
+    addDeptimeSec()
     createIndex()
