a981522f317b626389d7749c9e5e4a1269780abe
carl4498 <carl4498@umn.edu>
2018-12-04 14:55:39 -0600

    modify string read, add progress bar

--- blockAccessExtractor.py
+++ blockAccessExtractor.py
@@ -4,6 +4,7 @@
 
 #This program should be placed in the folder where the .csv files reside.
 
+#Input for blocks should look like this:
 
 ####################################
 ## Libraries and global variables ##
@@ -14,19 +15,13 @@ import datetime
 import os
 import time
 import argparse
+from myToolsPackage.progress import bar
 
-#Start timing
-start_time = time.time()
-#Make a variable for the current time for use in writing files.
-currentTime = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-#First 5 are the blockID's of blocks expected to see accessibility changes, the last five are not
-#expected to see a change.
-# Prompt user for a list of comma separated label IDs.
-blockID = input(['Comma separated list of labels'])
+
 
 #blockID = [270030507042007, 270531097002008, 270531261003037, 270530220002002, 271230333001014]
-#Read in the file of the baseline accessibility.
-def makeAccessOutput(access_file, list, filename):
+#Read in the file of the baseline and updated min-by-min accessibility.
+def makeAccessOutput(access_file, list, bar, filename):
     access_list = csv.reader(access_file, delimiter=',')
     with open("{0}_{1}.csv".format(filename, currentTime), 'wt') as access_extract: #, newline=''
         extract = csv.writer(access_extract, delimiter= ',')
@@ -35,8 +30,10 @@ def makeAccessOutput(access_file, list, filename):
         #Skips the first line of input file which is a header
         access_file.readline()
         for row in access_list:
+            bar.next()
             for i in list:
                 if i == int(row[0]):
+                    print('Found 1!')
                     #If there are other jobs categories, add them after "W_C000."
                     entry = [int(row[0]), int(row[1]), int(row[2]), int(row[3])]
                     extract.writerow(entry)
@@ -44,8 +41,11 @@ def makeAccessOutput(access_file, list, filename):
 if __name__ == "__main__":
     # Start timing
     start_time = time.time()
+    print(start_time)
     # Make a variable for the current time for use in writing files.
     currentTime = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    #Progress bar
+    bar = bar.Bar(message ='Processing', fill='@', suffix='%(percent)d%%', max=351864000) #54000 origins x 18 thresholds x 181 departure times x 2 files, 1.78 hours run time
     # Parameterize file paths: use argparse so that when blockAccessExtractor is run, the user adds additional arguments for the
     # necesarry file paths.
     parser = argparse.ArgumentParser()
@@ -53,13 +53,27 @@ if __name__ == "__main__":
     parser.add_argument('-updateaccess', '--UPDATE_ACCESS', required=True, default=None)
     args = parser.parse_args()
 
+    # Prompt user for a list of comma separated label IDs.
+    input_string = input('space separated labels') #enter like 270530234002015 271230425012030 270530240033002
+    list = input_string.split()
+    print('list', list)
+    blockID = []
+    for i in list:
+        blockID.append(int(i))
+    print('Block IDs', blockID)
+    #blockID = [int(x) for x in input('Comma separated list of labels w or without brackets?').split()]
+    # print("Your input")
+    # print(blockID)
+
     #Produces a new file where the "labels" or blockIDs that match the input above are extracted from the baseline
     #accessibility calc file for the given scenario.
     with open(os.path.join(args.BASE_ACCESS)) as base_file: #, newline=''
-        makeAccessOutput(base_file, blockID, "base")
+        makeAccessOutput(base_file, blockID, bar, "base")
         print("Baseline accessibility for select block groups")
     #Produces a new file with accessibility calcs extracted for the comparable blocks examined in the baseline extract.
     with open(os.path.join(args.UPDATE_ACCESS)) as update_file: #, newline=''
-        makeAccessOutput(update_file, blockID, "update")
+        makeAccessOutput(update_file, blockID, bar, "update")
         print("Changed accessibility for select block groups")
+        bar.finish()
+    print('End Time:', time.time())
 
