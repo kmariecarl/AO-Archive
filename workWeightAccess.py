7c79ad7148351189819ef8539b1f5bebd29d0df2
carl4498 <carl4498@umn.edu>
2018-12-04 15:04:09 -0600

    More user friendly, inputs from processed access results and rac data

--- workWeightAccess.py
+++ workWeightAccess.py
@@ -1,5 +1,5 @@
-#This script reads in a file that lists MN blocks and the associated job and worker values (taken from
-#MN_WAC_2014 and MN_RAC_2014 datasets that were joined in QGIS "Origins_852_156_TC_jobs_workers."
+#This script reads in an accessibility file that has been processed by 'processAccessResults4.py' and another file that
+#maps the analysis zone to RAC values and computes the worker weighted average for the selected accessibility threshold
 
 #This program stays in the "Programs" file because it imports "average" from numpy.
 
@@ -7,55 +7,66 @@
 #   GLOBAL VARIABLES & PACKAGES
 # ---------------------------------
 
-import csv
-import os
 from numpy import average
 import argparse
+from myToolsPackage import matrixLinkModule as mod
 
 # --------------------------
 #       FUNCTIONS
 # --------------------------
 
-def makeInputDict(access_file, access_field, worker_field):
-    with open(access_file, 'r') as input:
-        reader = csv.DictReader(input)
-        access_list = []
-        worker_list = []
-        print('Building Lists...')
+def lineUpLists(access_file, label1, access_field, worker_field):
+
+    access_list = []
+    worker_list = []
+    print('Building Lists...')
 
 
+    #This is an inefficient way to cycle through .csv info but I don't feel like making a dictionary structure beyond the DictReader obj.
+    for row in access_file:
 
-        for row in reader:
-        #This configuration ensures that the access and worker values are in the same order for each list.
-        #if row['raw_base_3'] not in access_list:
-            if row[access_field] is '':
-                access_list.append(0)
-            else:
-                access_list.append(float(row[access_field]))
-        #if row['rac_C000'] not in worker_list:
-            if row[worker_field] is '':
-                worker_list.append(0)
-            else:
-                worker_list.append(int(row[worker_field]))
-        print('Lists Returned')
-        return access_list, worker_list
+        #account for missing accessibility value
+        if row[access_field] is '' and row[worker_field] is '':
+            access_list.append(0)
+            worker_list.append(0)
+        elif row[access_field] is '':
+            access_list.append(0)
+            worker_list.append(int(row[worker_field]))
+            #Account for missing RAC value
+        elif row[worker_field] is '':
+            access_list.append(float(row[access_field]))
+            worker_list.append(0)
+            #No missing value option
+        else:
+            access_list.append(float(row[access_field]))
+            worker_list.append(int(row[worker_field]))
+
+    print('Lists Returned')
+    return access_list, worker_list
 
 
-
-
-#Give the path of the file that contains the pre-calculated accessibility values and the RAC values at the block level.
-#ACCESS = os.path.join('..', 'TestScenarios', 'mnpass_crnt_xpres', 'Post_process_access','tl_27_55_mnpass_crnt_xpres_impact_zones.csv')
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-r', '--ACCESS_FILE', required=True, default=None)
+    parser.add_argument('-access', '--ACCESS_FILE', required=True, default=None)
+    parser.add_argument('-rac', '--RAC_FILE', required=False, default=None) #Not required, program not setup for seperate RAC
     args = parser.parse_args()
 
-    # Prompt user for a time window.
-    access_field = input('Type field name that you want to find the worker weighted accessibility value for, i.e. rwbs1800:')
-    worker_field = input('Type field name containing number of workers, i.e. rac_C000:')
+    #Read in files to dict
+    access_file = mod.readInToDict(args.ACCESS_FILE)
 
-    accessList, workerList = makeInputDict(args.ACCESS_FILE, access_field, worker_field)
+    print("Accessibility (and/or RAC) fields to choose from:")
+    print(access_file.fieldnames)
+
+    # Prompt user for a time window.
+    label1 = input('Type the name of the field that corresponds with the row labels, i.e. TAZ, label, ID ')
+    access_field = input('Type field name that you want to find the worker weighted accessibility value for, i.e. rwbs1800: ')
+
+    worker_field = input('Type field name containing number of workers, i.e. rac_C000: ')
+
+
+    accessList, workerList = lineUpLists(access_file, label1, access_field, worker_field)
+    #Formula for weighted access. sum of interaction terms of access values by population divided by the sum of population (total pop).
     weighted_avg = average(accessList, weights=workerList)
     print('Worker-weighted Avg.:', weighted_avg)
