f82630c7bcc668db84019214a56d85a0c107187a
carl4498 <carl4498@umn.edu>
2018-12-04 14:52:08 -0600

    modify program input structure

--- addRAC.py
+++ addRAC.py
@@ -26,28 +26,31 @@ if __name__ == '__main__':
 
     # Parameterize file paths
     parser = argparse.ArgumentParser()
-    parser.add_argument('-file1', '--RAC_FILE_1', required=True, default=None)  #ENTER AS full file path to path_analyst file
-    parser.add_argument('-file2', '--RAC_FILE_2', required=True, default=None)  #ENTER AS full file path to path_analyst file
+    parser.add_argument('-file1', '--RAC_FILE_1', required=True, default=None)  #ENTER AS full file path to LEHD file
+    parser.add_argument('-file2', '--RAC_FILE_2', required=True, default=None)  #ENTER AS full file path to LEHD file
+    parser.add_argument('-wac_rac', '--WAC_RAC', required=True, default=None)  #Specify wac or rac being calculated
+    parser.add_argument('-label', '--LABEL', required=True, default=None)  #WAC is w_geocode and RAC is h_geocode
     parser.add_argument('-field', '--FIELD', required=True, default=None)  #output field name i.e. racC000 or wacC000
     args = parser.parse_args()
-
-    fieldnames = ['GEOID10','{}'.format(args.FIELD)]
-    writer = mod.mkDictOutput('Joined_RAC_FILE_{}'.format(curtime), fieldname_list=fieldnames)
 
 
     reader1 = mod.readInToDict(args.RAC_FILE_1)
     reader2 = mod.readInToDict(args.RAC_FILE_2)
+    label = args.LABEL
+    wac_rac = args.WAC_RAC
 
+    fieldnames = ['GEOID10','{}'.format(args.FIELD)]
+    writer = mod.mkDictOutput('Joined_{}_FILE_{}'.format(wac_rac, curtime), fieldname_list=fieldnames)
 
     for row in reader1:
 
-        entry = {'GEOID10': row['h_geocode'], '{}'.format(args.FIELD): row['C000']}
+        entry = {'GEOID10': row['{}'.format(label)], '{}'.format(args.FIELD): row['C000']}
 
         writer.writerow(entry)
     bar.next()
 
     for row2 in reader2:
-        entry = {'GEOID10': row2['h_geocode'], '{}'.format(args.FIELD): row2['C000']}
+        entry = {'GEOID10': row2['{}'.format(label)], '{}'.format(args.FIELD): row2['C000']}
 
         writer.writerow(entry)
     bar.next()
